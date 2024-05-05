a9aa5134bf5549a4b563849727cd9ffff1cca352
RRG <148712737+aitwehrrg@users.noreply.github.com>
2024-05-05 09:37:38 +0000

    Added Polish Stack Programs

---
+++ Data Structures/Stacks/polish.cpp
@@ -0,0 +1,147 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Polish
+{
+    stack<float> operands;
+    inline bool isDigit(char);
+    inline bool isOperator(char);
+    int precedence(char);
+    float operation(float, float, char);
+
+    public:
+    float prefix(string);
+    float postfix(string);
+    float infix(string);
+};
+
+inline bool Polish::isDigit(char c) { return c >= '0' && c <= '9'; }
+
+inline bool Polish::isOperator(char c) { return c == '+' || c == '-' || c == '*' || c == '/' || c == '^'; }
+
+int Polish::precedence(char c)
+{
+    switch(c)
+    {
+        case '^':
+        return 3;
+
+        case '*':
+        case '/':
+        return 2;
+
+        case '+':
+        case '-':
+        return 1;
+    }
+    return -1;
+}
+
+float Polish::operation(float a, float b, char c)
+{
+    switch(c)
+    {
+        case '+':
+        return a + b;
+
+        case '-':
+        return a - b;
+
+        case '*':
+        return a * b;
+
+        case '/':
+        return a / b;
+
+        case '^':
+        return pow(a, b);
+    }
+    cerr << "Invalid operator." << endl;
+    return -1;
+}
+
+float Polish::prefix(string expression) {
+    for(int i = expression.size() - 1; i >= 0; i--)
+        if(isDigit(expression[i]))
+            operands.push(expression[i] - '0');
+        else if(isOperator(expression[i])) {
+            float op1 = operands.top();   operands.pop();
+            float op2 = operands.top();   operands.pop();
+            operands.push(operation(op1, op2, expression[i]));
+        } else if(expression[i] == ' ')
+            continue;
+        else {
+            cerr<<"Invalid expression."<<endl;
+            return -1;
+        }
+    return operands.top();
+}
+
+float Polish::postfix(string expression) {
+    for(char c : expression)
+        if(isDigit(c))
+            operands.push(c - '0');
+        else if(isOperator(c)) {
+            float op2 = operands.top();   operands.pop();
+            float op1 = operands.top();   operands.pop();
+            operands.push(operation(op1, op2, c));
+        } else if(c == ' ')
+            continue;
+        else {
+            cerr<<"Invalid expression."<<endl;
+            return -1;
+        }
+    return operands.top();
+}
+
+float Polish::infix(string expression) {
+    stack<char> operators;
+    string post = "";
+
+    for(char c : expression) {
+        if(isDigit(c))
+            post += c;
+        else if(c == '(')
+            operators.push(c);
+        else if(c == ')') {
+            while(operators.top() != '(') {
+                post += operators.top();
+                operators.pop();
+            }
+            operators.pop();
+        } else if(isOperator(c)) {
+            while(!operators.empty() && precedence(c) <= precedence(operators.top())) {
+                post += operators.top();
+                operators.pop();
+            }
+            operators.push(c);
+        } else if(c == ' ')
+            continue;
+        else {
+            cerr << "Invalid expression." << endl;
+            return -1;
+        }
+    }
+    while(!operators.empty()) {
+        post += operators.top();
+        operators.pop();
+    }
+    return postfix(post);
+}
+
+
+int main() {
+    Polish polish;
+    string expression;
+    
+    cout << "Enter prefix expression: "; getline(cin,expression);
+    cout << "Result: "<<polish.prefix(expression) << endl;
+
+    cout << "Enter postfix expression: "; getline(cin,expression);
+    cout << "Result: "<<polish.postfix(expression) << endl;
+
+    cout << "Enter infix expression: "; getline(cin,expression);
+    cout << "Result: "<<polish.infix(expression) << endl;
+    
+    return 0;
+}
---
+++ Data Structures/Stacks/polishConversion.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+inline bool isOperator(char c) { return c == '^' || c == '*' || c == '/' || c == '+' || c == '-'; }
+
+inline bool isDigit(char c) { return c >= 'A' && c <= 'Z'; }
+
+int precedence(char c) {
+    switch (c) {
+        case '^': return 3;
+        case '*': case '/': return 2;
+        case '+': case '-': return 1;
+    }
+    return -1;
+}
+
+float evaluate(float a, float b, char c) {
+    switch (c) {
+        case '^': return pow(a, b);
+        case '*': return a * b;
+        case '/': return a / b;
+        case '+': return a + b;
+        case '-': return a - b;
+    }
+    cerr << "Invalid operator." << endl;
+    return -1;
+}
+
+string infixToPostfix(string infix) {
+    stack<char> operators;
+    string result = "";
+
+    for (char c : infix) {
+        if (isDigit(c))
+            result += c;
+        else if (isOperator(c)) {
+            while (!operators.empty() && precedence(operators.top()) >= precedence(c)) {
+                result += operators.top();
+                operators.pop();
+            }
+            operators.push(c);
+        } else if (c == '(')
+            operators.push(c);
+        else if (c == ')') {
+            while (!operators.empty() && operators.top() != '(') {
+                result += operators.top();
+                operators.pop();
+            }
+            operators.pop();
+        } else if (c == ' ')
+            continue;
+        else {
+            cerr << "Invalid character." << endl;
+            return "";
+        }        
+    }
+
+    while (!operators.empty()) {
+        result += operators.top();
+        operators.pop();
+    }
+    return result;
+}
+
+string infixToPrefix(string infix) {
+    reverse(infix.begin(), infix.end());
+
+    string result = infixToPostfix(infix);
+
+    reverse(result.begin(), result.end());    
+    return result;
+}
+
+int main() {
+    string infix, postfix, prefix;
+    cout << "Enter infix expression: "; getline(cin, infix);
+    postfix = infixToPostfix(infix);
+    cout << "Postfix: " << postfix << endl;
+
+    prefix = infixToPrefix(infix);
+    cout << "Prefix: " << prefix << endl;
+    return 0;
+}
