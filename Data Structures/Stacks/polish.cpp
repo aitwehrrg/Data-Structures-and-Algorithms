33c6bb18ce43a5eefff18b4b2584b08f10ab819a
aitwehrrg <rrginfinity@gmail.com>
2024-06-03 11:17:26 +0530

    Fixed spacing

--- Data Structures/Stacks/polish.cpp
+++ Data Structures/Stacks/polish.cpp
@@ -56,34 +56,34 @@ float Polish::operation(float a, float b, char c) const {
 }
 
 float Polish::prefix(string expression) {
-    for(int i = expression.size() - 1; i >= 0; i--)
-        if(isDigit(expression[i]))
+    for (int i = expression.size() - 1; i >= 0; i--)
+        if (isDigit(expression[i]))
             operands.push(expression[i] - '0');
-        else if(isOperator(expression[i])) {
-            float op1 = operands.top();   operands.pop();
-            float op2 = operands.top();   operands.pop();
+        else if (isOperator(expression[i])) {
+            float op1 = operands.top(); operands.pop();
+            float op2 = operands.top(); operands.pop();
             operands.push(operation(op1, op2, expression[i]));
-        } else if(expression[i] == ' ')
+        } else if (expression[i] == ' ')
             continue;
         else {
-            cerr<<"Invalid expression."<<endl;
+            cerr << "Invalid expression." << endl;
             return -1;
         }
     return operands.top();
 }
 
 float Polish::postfix(string expression) {
-    for(char c : expression)
-        if(isDigit(c))
+    for (char c : expression)
+        if (isDigit(c))
             operands.push(c - '0');
-        else if(isOperator(c)) {
-            float op2 = operands.top();   operands.pop();
-            float op1 = operands.top();   operands.pop();
+        else if (isOperator(c)) {
+            float op2 = operands.top(); operands.pop();
+            float op1 = operands.top(); operands.pop();
             operands.push(operation(op1, op2, c));
-        } else if(c == ' ')
+        } else if (c == ' ')
             continue;
         else {
-            cerr<<"Invalid expression."<<endl;
+            cerr << "Invalid expression." << endl;
             return -1;
         }
     return operands.top();
@@ -93,31 +93,31 @@ float Polish::infix(string expression) {
     stack<char> operators;
     string post = "";
 
-    for(char c : expression) {
-        if(isDigit(c))
+    for (char c : expression) {
+        if (isDigit(c))
             post += c;
-        else if(c == '(')
+        else if (c == '(')
             operators.push(c);
-        else if(c == ')') {
-            while(operators.top() != '(') {
+        else if (c == ')') {
+            while (operators.top() != '(') {
                 post += operators.top();
                 operators.pop();
             }
             operators.pop();
-        } else if(isOperator(c)) {
-            while(!operators.empty() && precedence(c) <= precedence(operators.top())) {
+        } else if (isOperator(c)) {
+            while (!operators.empty() && precedence(c) <= precedence(operators.top())) {
                 post += operators.top();
                 operators.pop();
             }
             operators.push(c);
-        } else if(c == ' ')
+        } else if (c == ' ')
             continue;
         else {
             cerr << "Invalid expression." << endl;
             return -1;
         }
     }
-    while(!operators.empty()) {
+    while (!operators.empty()) {
         post += operators.top();
         operators.pop();
     }
