0aa0a3a521b6cc7f55ae81f992472d7d01058b19
aitwehrrg <rrginfinity@gmail.com>
2024-06-03 10:19:57 +0530

    Added recursive Binary Search

---
+++ Algorithms/Recursion/binarySearch.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> T binarySearch(vector<T>& arr, T key, unsigned int low, unsigned int high) {
+    if (low > high) return -1;
+    
+    int mid = (low + high) / 2;
+    if (arr[mid] == key) return mid;
+    if (arr[mid] > key) return binarySearch(arr, key, low, mid - 1);
+    return binarySearch(arr, key, mid + 1, high);
+}
